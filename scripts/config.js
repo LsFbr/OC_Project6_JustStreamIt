6e8fb4a67d08b3352e27ea0d8e38f373febd5c6e
LsFbr <louisfabre@hotmail.fr>
2025-02-21 17:43:39 +0100

    fix(config.js): wrong values for CATEGORY_2 and CATEGORY_3: wrong category names.

--- scripts/config.js
+++ scripts/config.js
@@ -1,6 +1,6 @@
 const API_BASE_URL = 'http://localhost:8000/api/v1';
 
 const CATEGORY_1 = 'Action';
-const CATEGORY_2 = 'Comédie';
-const CATEGORY_3 = 'Drame';
+const CATEGORY_2 = 'Comedy';
+const CATEGORY_3 = 'Drama';
 
