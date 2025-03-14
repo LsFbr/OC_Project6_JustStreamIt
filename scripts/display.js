d51ff4cb8c708257b09bd79913954cc214dbdcb8
LsFbr <louisfabre@hotmail.fr>
2025-03-14 12:34:39 +0100

    feat(display.js): create displayModal(movieId) and closeModal() functions.

--- scripts/display.js
+++ scripts/display.js
@@ -1,11 +1,24 @@
 document.addEventListener('DOMContentLoaded',displayBestMovie);
 document.addEventListener('DOMContentLoaded',displayCategoryMovies);
 document.addEventListener('DOMContentLoaded',displayCustomCategoryChoices);
-document.addEventListener('DOMContentLoaded',setupShowMoreButtons);
 document.addEventListener('DOMContentLoaded',displayCategoryTitles);
+document.addEventListener('DOMContentLoaded',setupShowMoreButtons);
 
 const selectElement = document.getElementById('cat-custom-select');
 selectElement.addEventListener('change', displayCustomCategoryMovies);
+
+document.addEventListener('click', (event) => {
+    if (event.target.classList.contains('details-button')) {
+        const movieId = event.target.getAttribute('movie-id');
+        displayModal(movieId);
+    }
+});
+
+document.addEventListener('click', (event) => {
+    if (event.target.id === 'close-modal') {
+        closeModal();
+    }
+});
 
 function setupShowMoreButtons() {
     const categoryIdentifiers = ['cat-1', 'cat-2', 'cat-3', 'cat-custom'];
@@ -56,6 +69,9 @@ async function displayBestMovie() {
     
     document.querySelector('#meilleur-film-description h3').textContent = movie.title;
     document.querySelector('#meilleur-film-description p').textContent = movie.description;
+
+    const detailsButton = document.querySelector('#meilleur-film-content .details-button');
+    detailsButton.setAttribute('movie-id', movie.id);
 }
 
 async function displayCategory1Movies() {
@@ -64,6 +80,7 @@ async function displayCategory1Movies() {
 
     for (let i = 0; i < movies.length; i++) {
         const movie = movies[i];
+
         const imgElement = document.querySelector(`#cat-1-${i + 1} img`);
         imgElement.onerror = () => {
             imgElement.src = 'images/no_image.png';
@@ -72,6 +89,9 @@ async function displayCategory1Movies() {
         imgElement.alt = `Affiche du film ${movie.title}`;
 
         document.querySelector(`#cat-1-${i + 1} h3`).textContent = movie.title;
+
+        const detailsButton = document.querySelector(`#cat-1-${i + 1} .details-button`);
+        detailsButton.setAttribute('movie-id', movie.id);
     }
 }
 
@@ -81,6 +101,7 @@ async function displayCategory2Movies() {
 
     for (let i = 0; i < movies.length; i++) {
         const movie = movies[i];
+
         const imgElement = document.querySelector(`#cat-2-${i + 1} img`);
         imgElement.onerror = () => {
             imgElement.src = 'images/no_image.png';
@@ -89,6 +110,9 @@ async function displayCategory2Movies() {
         imgElement.alt = `Affiche du film ${movie.title}`;
 
         document.querySelector(`#cat-2-${i + 1} h3`).textContent = movie.title;
+        
+        const detailsButton = document.querySelector(`#cat-2-${i + 1} .details-button`);
+        detailsButton.setAttribute('movie-id', movie.id);
     }
 }
 
@@ -98,7 +122,9 @@ async function displayCategory3Movies() {
 
     for (let i = 0; i < movies.length; i++) {
         const movie = movies[i];
+
         const imgElement = document.querySelector(`#cat-3-${i + 1} img`);
+        
         imgElement.onerror = () => {
             imgElement.src = 'images/no_image.png';
         };
@@ -106,6 +132,9 @@ async function displayCategory3Movies() {
         imgElement.alt = `Affiche du film ${movie.title}`;
 
         document.querySelector(`#cat-3-${i + 1} h3`).textContent = movie.title;
+
+        const detailsButton = document.querySelector(`#cat-3-${i + 1} .details-button`);
+        detailsButton.setAttribute('movie-id', movie.id);
     }
 }
 
@@ -124,6 +153,10 @@ async function displayCustomCategoryMovies() {
         imgElement.alt = `Affiche du film ${movie.title}`;
 
         document.querySelector(`#cat-custom-${i + 1} h3`).textContent = movie.title;
+
+        const detailsButton = document.querySelector(`#cat-custom-${i + 1} .details-button`);
+        detailsButton.setAttribute('movie-id', movie.id);
+
     }
 }
 
@@ -146,4 +179,85 @@ async function displayCustomCategoryChoices() {
         optionElement.classList.add('font-semibold');
         selectElement.appendChild(optionElement);
     }
+}
+
+async function displayModal(movieId) {
+    try {
+        const movieDetails = await fetchMovieDetails(movieId);
+        if (!movieDetails) {
+            throw new Error('Aucun détail de film trouvé');
+        }
+
+        const modal = document.getElementById('modal');
+        modal.classList.remove('hidden');
+
+        const modalTitle = document.getElementById('modal-title');
+        modalTitle.textContent = movieDetails.title;
+
+        const modalYear = document.getElementById('modal-year');
+        modalYear.textContent = movieDetails.year;
+
+        const modalGenres = document.getElementById('modal-genres');
+        modalGenres.textContent = "- " + movieDetails.genres.join(', ');
+
+        const modalRated = document.getElementById('modal-rated');
+        if (Number.isInteger(parseInt(movieDetails.rated,10))) {
+            modalRated.textContent = "PG-" + movieDetails.rated;
+        } else {
+            modalRated.textContent = "Not rated or unknown rating";
+        }
+
+        const modalDuration = document.getElementById('modal-duration');
+        modalDuration.textContent = "- " + movieDetails.duration + " minutes";
+
+        const modalCountries = document.getElementById('modal-countries');
+        modalCountries.textContent = " (" + movieDetails.countries.join(' / ') + ")";
+
+        const modalImdbScore = document.getElementById('modal-imdb-score');
+        modalImdbScore.textContent = "IMDB Score: " + movieDetails.imdb_score + "/10";
+
+        const modalGrossIncome = document.getElementById('modal-gross-income');
+        modalGrossIncome.textContent = "Recette au box-office: " + movieDetails.worldwide_gross_income;
+
+        const modalDirectors = document.getElementById('modal-directors');
+        if (modalDirectors) {
+            const directorsLabel = document.getElementById('modal-directors-label');
+            directorsLabel.textContent = "Réalisé par:";          
+            modalDirectors.textContent = movieDetails.directors.join(', ');
+        } else {
+            directorsLabel.textContent = "";
+        }
+
+        const modalLongDescription = document.getElementById('modal-long-description');
+        modalLongDescription.textContent = movieDetails.long_description;
+
+        const modalImage = document.getElementById('modal-image');
+        modalImage.onerror = () => {
+            modalImage.src = 'images/no_image.png';
+        };
+        modalImage.src = movieDetails.image_url;
+        modalImage.alt = `Affiche du film ${movieDetails.title}`;
+
+        const modalActors = document.getElementById('modal-actors');
+        if (modalActors) {
+            const actorsLabel = document.getElementById('modal-actors-label');
+            actorsLabel.textContent = "Avec:";
+            modalActors.textContent = movieDetails.actors.join(', ');
+        } else {
+            actorsLabel.textContent = "";
+        }
+
+
+
+
+
+        modal.classList.remove('hidden');    
+    } catch (error) {
+        console.error('Erreur lors de la récupération des détails du film:', error);
+    }   
+}
+
+function closeModal() {
+    const modal = document.getElementById('modal');
+    modal.classList.add('hidden');
 }
